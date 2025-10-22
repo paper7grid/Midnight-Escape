1076d29b08b17837bb047b72133219bd8611a886
paper7grid <muddassirsadia123@gmail.com>
2025-10-21 17:11:14 -0700

    Update skeleton.gd

--- midnight-escape/Scripts/skeleton.gd
+++ midnight-escape/Scripts/skeleton.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 const GRAVITY : int = 4200
 const JUMP_SPEED : int = -1800
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_SPEED
-		$AudioStreamPlayer2D.play()
-		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+			#$AudioStreamPlayer2D.play()
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("Duck?")
+		else: 
+			$AnimatedSprite2D.play("Walk")
+	else:
+		$AnimatedSprite2D.play("Idle")
+	
+	move_and_slide()
 	
