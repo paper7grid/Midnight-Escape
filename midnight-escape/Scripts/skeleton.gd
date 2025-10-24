deebed9365220eb29fc8bec5eb016696fae2121d
paper7grid <muddassirsadia123@gmail.com>
2025-10-24 09:49:32 -0700

    disabling the collision shape

--- midnight-escape/Scripts/skeleton.gd
+++ midnight-escape/Scripts/skeleton.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var run_col: CollisionShape2D = $Run_col
 
 const GRAVITY : int = 6200
 const JUMP_SPEED : int = -1800
@@ -9,11 +10,13 @@ const JUMP_SPEED : int = -1800
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
+		$Run_col.disabled = false
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_SPEED
 			#$AudioStreamPlayer2D.play()
 		elif Input.is_action_pressed("ui_down"):
 			$AnimatedSprite2D.play("Duck?")
+			$Run_col.disabled = true
 		else: 
 			$AnimatedSprite2D.play("Walk")
 	else:
